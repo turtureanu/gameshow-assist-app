0adde0f9c66748d0e1ab35ddf49b356ef0deda7e
turtureanu <90131332+turtureanu@users.noreply.github.com>
2024-08-10 12:11:03 +0200

    fix(index): overflow when the screen is too small

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,15 @@
 </script>
 
 <div
-  class="flex justify-center items-center h-full gap-x-4 text-3xl"
+  class="flex flex-col md:flex-row justify-center items-center h-full gap-4 text-3xl"
   aria-label="menu"
 >
   <NavLink
-    class="outline-6 hover:outline hover:outline-lightblue p-20 bg-gradient-to-tr bg-gradient-from-teal-100 bg-gradient-to-blue-200"
+    class="outline-6 hover:outline hover:outline-lightblue p-16 bg-gradient-to-tr bg-gradient-from-teal-100 bg-gradient-to-blue-200"
     href="/keep-the-score">Keep the Score</NavLink
   >
   <NavLink
-    class="outline-6 hover:outline hover:outline-red p-20 bg-gradient-to-tr bg-gradient-from-violet-100 bg-gradient-to-red-200"
+    class="outline-6 hover:outline hover:outline-red p-16 bg-gradient-to-tr bg-gradient-from-violet-100 bg-gradient-to-red-200"
     href="/wheel">Spin the wheel</NavLink
   >
 </div>
